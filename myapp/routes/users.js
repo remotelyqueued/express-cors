27794c176e223264d75045ecd8afabc87eb5e2da
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-02 15:49:46 -0700

    Minor changes

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -6,16 +6,16 @@ var userJSON = require('../users/user.json');
 // in app.js set route "/users"
 // in users.js route is "/"
 router.all('/', function (req, res, next) {
-    console.log('');
-    console.log(req.protocol);
-    console.log(req.hostname);
-    console.log(req.path);
-    console.log(req.originalUrl);
-    console.log('');
-    console.log(req.subdomains);
-    console.log(req.body);
-    console.log(req.method);
-    console.log('');
+    // console.log('');
+    // console.log(req.protocol);
+    // console.log(req.hostname);
+    // console.log(req.path);
+    // console.log(req.originalUrl);
+    // console.log('');
+    // console.log(req.subdomains);
+    // console.log(req.body);
+    // console.log(req.method);
+    // console.log('');
     console.log(req.header('user-agent'));
     console.log(req.header('Content-Type'));
     res.json(userJSON);
